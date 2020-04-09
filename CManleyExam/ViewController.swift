2728c8df56af86c431086f34cf461a96be35ccbe
Claire Manley <cmanley2@zagmail.gonzaga.edu>
2020-04-08 22:32:41 -0700

    Everything in my app works perfectly except for Bernie Sanders, which is a little fitting for the times. So maybe this was purposeful? ;)

--- CManleyExam/ViewController.swift
+++ CManleyExam/ViewController.swift
@@ -8,12 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
 
+{
+
+
+    @IBOutlet weak var label0: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    
+    @IBOutlet weak var imageView0: UIImageView!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var imageView5: UIImageView!
+    
+    var candidate = ["Warren", "Sanders", "Klobuchar", "Buttigeg", "Bloomberg", "Biden"]
+    
+    var voteCount = [0,0,0,0,0,0]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label0.text = "Warren: \(voteCount[0])"
+        label1.text = "Sanders: \(voteCount[1])"
+        label2.text = "Klobuchar: \(voteCount[2])"
+        label3.text = "Buttigeg: \(voteCount[3])"
+        label4.text = "Bloomberg: \(voteCount[4])"
+        label5.text = "Biden: \(voteCount[5])"
+        
+        imageView0.image = UIImage(named: "Warren")
+        imageView1.image = UIImage(named: "Sanders")
+        imageView2.image = UIImage(named: "Klobuchar")
+        imageView3.image = UIImage(named: "Buttigeg")
+        imageView4.image = UIImage(named: "Bloomberg")
+        imageView5.image = UIImage(named: "Biden")
+        
     }
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        if sender.tag == 0 {
+            voteCount[0] = Int(sender.value)
+            label0.text = "Warren: " + String(sender.value)
+        
+        }
+        if sender.tag == 1 {
+        voteCount[1] = Int(sender.value)
+        label1.text = "Sanders: " + String(sender.value)
+        
+        }
+        if sender.tag == 2 {
+        voteCount[2] = Int(sender.value)
+        label2.text = "Klobuchar: " + String(sender.value)
+    
+        }
+        if sender.tag == 3 {
+        voteCount[3] = Int(sender.value)
+        label3.text = "Buttigeg: " + String(sender.value)
+            
+        }
+        if sender.tag == 4 {
+        voteCount[4] = Int(sender.value)
+        label4.text = "Bloomberg: " + String(sender.value)
+        }
+        if sender.tag == 5 {
+        voteCount[5] = Int(sender.value)
+            label5.text = "Biden: " + String(sender.value)}
+        
+    }
+    
 
 
 }
